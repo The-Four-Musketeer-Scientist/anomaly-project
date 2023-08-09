b03c6ef14ee6fe4a010c67d9639a87aaafcb9246
Jonathan Ware <jonathan.ware02@gmail.com>
2023-08-09 12:24:52 -0500

    edit wrangle

--- wrangle.py
+++ wrangle.py
@@ -1,32 +1,65 @@
 import pandas as pd
 import numpy as np
+from env import get_db_url
 import os
 
 
-def get_log_data():
-    df = pd.read_csv('anonymized-curriculum-access.txt', sep=' ', header=None,
-                     names=['Timestamp', 'Resource', 'User_ID', 'Access_Type', 'IP_Address'])
-    return df
+
+
+
+
+def new_log_data():
+   
+    conn = get_db_url('curriculum_logs')
+
+    query = '''
+            SELECT logs.*, cohorts.*
+            FROM logs
+            LEFT JOIN cohorts ON logs.cohort_id = cohorts.id;
+
+            '''
+
     
+    df = pd.read_sql(query, conn)
+    return df
+    """
+    Retrieves curriculum log data either from a CSV file or by calling the new_log_data function.
+    If the data exists in a CSV file, it is loaded from the file. Otherwise, it is retrieved
+    from the database and saved to the CSV file for future use.
+    
+    Returns:
+    - df: pandas DataFrame
+        DataFrame containing the Telco data.
+    """
+def get_log_data():
+    if os.path.isfile('log_df.csv'):
+        df = pd.read_csv('log_df.csv', index_col = 0)
+        
+        return df
 
+    else:
 
+        df = new_log_data()
 
-
+        df.to_csv('log_df.csv')
+        return df
 #---------------------------------------------PREP-----------------------------------------------------    
 
-def prep_log_data(df):
-    
+def prep_log_data():
     '''
-    This function preps the data from get_crime_data and does the necessary step to cleaning the dataframe
+    This function acquires and preps the log data.
     '''
-    
-    
-    df['date'] = pd.to_datetime(df['date'])
-    df.set_index('date', inplace=True) 
-    df = df.resample('D')['path'].count()
-    
-    
-    return df    
 
+    if os.path.isfile('log_df.csv'):
+        df = pd.read_csv('log_df.csv', index_col=0)
+    else:
+        df = new_log_data()
+        df.to_csv('log_df.csv')
+
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace=True)
+    
+
+    return df
 
 
