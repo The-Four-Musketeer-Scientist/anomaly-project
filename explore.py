f8268dd771ecf794afddfcc5bd7eef6e2a251845
Jonathan Ware <jonathan.ware02@gmail.com>
2023-08-10 14:05:08 -0500

    added visual notebook

--- explore.py
+++ explore.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 import seaborn as sns
+import os
 
 
 def one_user_df_prep(df, user):
@@ -68,3 +69,57 @@ def find_anomalies(df, user, span, weight, plot=False):
 
 
 
+def save_visuals(fig: plt.figure ,viz_name:str= "unamed_viz", folder_name:int= 0, ) -> str:
+    """
+    Goal: Save a single visual into the project visual folder
+    parameters:
+        fig: seaborn visual figure to be saved
+        viz_name: name of the visual to save
+        folder_name: interger (0-7)represanting the section you are on in the pipeline
+            0: all other (defealt)
+            1: univariate stats
+            2: bivariate stats
+            3: multivariate stats
+            4: stats test
+            5: modeling
+            6: final report
+            7: presantation
+    return:
+        message to user on save status
+    """
+    project_visuals = "./00_project_visuals"
+    folder_selection = {
+        0: "00_non_specific_viz",
+        1: "01_univariate_stats_viz",
+        2: "02_bivariate_stats_viz",
+        3: "03_multivariate_stats_viz",
+        4: "04_stats_test_viz",
+        5: "05_modeling_viz",
+        6: "06_final_report_viz",
+        7: "07_presantation"
+    }
+    # return error if user input for folder selection is not found
+    if folder_name not in list(folder_selection.keys()):
+        return f"{folder_name} is not a valid option for a folder name."
+    # when folder location is found in selections
+    else:
+        # Specify the path to the directory where you want to save the figure
+        folder_name = folder_selection[folder_name]
+        directory_path = f'{project_visuals}/{folder_name}/'
+        # Create the full file path by combining the directory path and the desired file name
+        file_path = os.path.join(directory_path, f'{viz_name}.png')
+        if os.path.exists(project_visuals): # check if the main viz folder exists
+            if not os.path.exists(directory_path): # check if the folder name already exists
+                os.makedirs(directory_path)
+                # Save the figure to the specified file path
+                fig.canvas.print_figure(file_path)
+            else:
+                # Save the figure to the specified file path
+                fig.canvas.print_figure(file_path)
+        else:
+            # create both the project vis folder and the specific section folder
+            os.makedirs(project_visuals)
+            os.makedirs(directory_path)
+            # Save the figure to the specified file path
+            fig.canvas.print_figure(file_path)
+    return f"Visual successfully saved in folder: {folder_name}"
